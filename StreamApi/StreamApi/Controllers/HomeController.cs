cc104349296183944d70cd4bc3325573e5ab99e3
Renan de Carvalho Ribeiro <renancr176@gmaail.com>
2024-06-11 15:24:43 -0300

    Created base projects

---
+++ StreamApi/StreamApi/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StreamApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        
+    }
+}
